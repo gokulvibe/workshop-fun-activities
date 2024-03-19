91ae796ccd5e569459a70f4f9c3839cc87c18b4b
Gokul Raam <gokul.raam@saama.com>
2024-03-20 00:51:49 +0530

    Added appropriate images

--- theme-switcher/src/components/Doge.jsx
+++ theme-switcher/src/components/Doge.jsx
@@ -2,6 +2,11 @@ import { THEME_TYPE } from "../constants";
 import {ThemeContext} from "../providers/ThemeProvider";
 import React, { useState, useContext } from "react";
 
+import angryDoge from "../images/angry doge.jpg"
+import happyDoge from "../images/happy doge.jpg"
+import disappointedDoge from "../images/Dissapointed doge.jpg"
+import dogeApproves from "../images/Doge approves.jpg"
+
 
 const Doge = () => {
     const [themeMode, setTheme] = useContext(ThemeContext);
@@ -11,12 +16,12 @@ const Doge = () => {
         <div className="doge-image">
             {
                 themeMode === THEME_TYPE.LIGHT ? (
-                    <img src="images/angry doge.jpg"></img>
+                    <img src={angryDoge}></img>
                 ) : themeMode === THEME_TYPE.GREEN ? (
-                    <img src="images/angry doge.jpg"></img>
+                    <img src={disappointedDoge}></img>
                 ) : themeMode === THEME_TYPE.DARK ? (
-                    <img src="images/angry doge.jpg"></img>
-                ) : <img src="images/angry doge.jpg"></img>
+                    <img src={dogeApproves}></img>
+                ) : <img src={happyDoge}></img>
             }
         </div>
     )
